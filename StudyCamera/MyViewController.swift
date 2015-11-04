98c06ab94395d74aa4504907b45b3d144937b838
kinjo Ryuta <e115706@jonny.local>
2015-11-04 21:53:14 +0900

    ボタンを押すとフォトライブラリーから写真を選択し、トップに表示させる

---
+++ StudyCamera/MyViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MyViewController.swift
+//  StudyCamera
+//
+//  Created by kinjo Ryuta on 2015/11/04.
+//  Copyright © 2015年 dreamarts. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyViewController : UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBAction func didTouchButton(sender: AnyObject) {
+        
+        let picker = UIImagePickerController()
+        picker.sourceType = .PhotoLibrary
+        picker.delegate = self
+        
+        self.presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            myImageView.image = image
+            
+        }
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
