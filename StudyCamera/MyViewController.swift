dcdf5da19e0be43f36648ce1f07500c5c054e474
Jonyi19 <rionerumessi624@gmail.com>
2015-11-16 20:52:56 +0900

    選んだ画像を配列で格納

--- StudyCamera/MyViewController.swift
+++ StudyCamera/MyViewController.swift
@@ -13,6 +13,41 @@ class MyViewController : UIViewController, UIImagePickerControllerDelegate, UINa
     
     @IBOutlet weak var myTableView: UITableView!
     
+    var images:[UIImage] = Array()
+    
+    var selectedRow:Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        
+        self.myTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "MyTableViewCell")
+
+    }
+    
+    //テーブルのセクションの数
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //cellの数の指定
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    //Cellの生成
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("myTableView", forIndexPath: indexPath) as! ImageTableViewCell
+      //  cell.textLabel?.text = "test"
+        
+       cell.myImageView.image = images[indexPath.row]
+        
+        return cell
+    }
+
+    
     
     @IBAction func didTouchButton(sender: AnyObject) {//ボタンを押すとフォトライブラリーを呼ぶ
         print("Hello")
@@ -25,10 +60,18 @@ class MyViewController : UIViewController, UIImagePickerControllerDelegate, UINa
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {//フォトライブラリーを画面にセット
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
-     //       myImageView.image = image
+            images.append(image)
+            myTableView.reloadData()
+            
+            //myTableView.image = image
             
         }
         picker.dismissViewControllerAnimated(true, completion: nil)
     }
     
+}
+
+class ImageTableViewCell : UITableViewCell{
+    
+    @IBOutlet weak var myImageView: UIImageView!
 }
